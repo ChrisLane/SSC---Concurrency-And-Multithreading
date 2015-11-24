54781693f6aea8e6e0aa7ff7343789df57419eeb
ChrisLane <ChrisLane@users.noreply.github.com>
2015-11-24 23:06:42 +0000

    Remove unneeded test class

--- src/main/java/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-
-    public static void main(String[] args) {
-        Download download = new Download("http://www.cs.bham.ac.uk/~dehghanh/vision_files/lab/lab5/", "downloads");
-        download.downloadAll();
-    }
-}
